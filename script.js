ab0b9989a84c89fcfda780a17825ba05a6f465f2
Keven Molina <kevenmolina@gmail.com>
2021-01-28 16:52:31 -0600

    email validation function added as well as it's respective error message, removed dead code

--- script.js
+++ script.js
@@ -1,51 +1,38 @@
-//Retrieve field in form element
-// const form = $(form)
-// console.log(document.documentElement.innerHTML)
-let inputValues = {};
-    //Check that all fields are  filled
-    function emptyInputCheck(){
-        for(let things in inputValues){
-            if(inputValues[things] == ""){
-                //return error message(append to respective field)
-            console.log(`${things} cannot be empty`);
-            }else{
-                //
-            }
-         }
+
+    //email validation
+    function emailValidation(str){
+        const atCheck = /\S+@\S+\.\S+/i;
+       return atCheck.test(str);
     }
 
-
-// Check that email field is properly field out
-$(document).ready(function(){
- 
+    $(document).ready(function(){
+        // Check that email field is properly field out
         $('#signup-form').submit(function(e){
-            //Values collected from form are reset
-            inputValues = {};
+            let emailInput = $('#form-input-Email');
             //Gather input values from sign-up form
             $('.input-text-field').each(function(){
-                // inputValues[$(this).attr('name')]=$(this).val();
-                console.log($(this).val())
-                //Remove any prior existing message
+            //Remove any prior existing message
                 if($(this).next().hasClass('errorMsg')){
-                    console.log("Error message Deteched");
-                    console.log($(this).next());
                     $(this).next().remove();
                 };
-                if($(this).val()==""){
-                    //append error message to input without user's input
-                    $('.signup-form-section form input').attr('margin-top','16px');
-                    $(`<p>${$(this).attr('name')} cannot be empty</p>`).addClass('errorMsg').insertAfter($(this))
+            //append error message to input without user's input
+            if($(this).val()==""){
+                $('.signup-form-section form input').attr('margin-top','16px');
+                $(`<p>${$(this).attr('name')} cannot be empty</p>`)
+                .addClass('errorMsg').insertAfter($(this));
                 }
-            })
-            //return the input in form
-            // emptyInputCheck(inputValues);
-            // $('')
+            });
+            //Email Vailidation
+            if(emailValidation(emailInput.val())===false&&emailInput.val() !==""){
+                $('.signup-form-section form input').attr('margin-top','16px');
+                $(`<p>Looks like this is not an email</p>`)
+                .addClass('errorMsg').insertAfter(emailInput);
+            };
         e.preventDefault();
     })
 
 
 
 })
-// console.log(inputValues) testing
 
 
