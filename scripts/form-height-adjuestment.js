a92643fe62007f1a114a963f0b144af687f853ac
Keven Molina <kevenmolina@gmail.com>
2021-03-29 12:57:36 -0500

    Add new script for form-adjustment,

---
+++ scripts/form-height-adjuestment.js
@@ -0,0 +1,26 @@
+'use strict';
+
+export { errorMsgCheck };
+/*On Form Submit,
+    check if form height has changed
+        - comparare currentHeight to recentHeight
+*/ 
+
+//For Each errror message -> add {error message } pixels of height to form section
+// listen for form submission
+// If there are error messages
+    //cache the amount and there height - useful during recheck
+
+    let signUpForm = $('#signup-form').innerHeight();
+
+//Functions
+    //errorCheck
+
+    //addHeight
+    //subtractHeight
+    export const errorMsgCheck =function (){
+            // let errorMessage = $('.errorMsg');
+            // console.log(errorMessage);
+            console.log("Hello");
+ 
+    }
---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+
+
+'use strict';
+    // import {errorMsgCheck} from './form-height-adjuestment.js';
+    //email validation
+    // console.log(errorMsgCheck)
+
+    function emailValidation(str){
+        const atCheck = /\S+@\S+\.\S+/i;
+       return atCheck.test(str);
+    }
+
+    $(document).ready(function(){
+        // Check that email field is properly field out
+        $('#signup-form').submit(function(e){
+            let emailInput = $('#form-input-Email');
+            //Gather input values from sign-up form
+            $('.input-text-field').each(function(){
+            //Remove any prior existing message
+                if($(this).next().hasClass('errorMsg')){
+                    $(this).next().remove();
+                };
+            //append error message to input without user's input
+            if($(this).val()==""){
+                $('.signup-form-section form input').attr('margin-top','16px');
+                //extend form as well by adding 16px to heigth
+                $('.signup-form-section form').attr('height','16px');
+                $(`<p>${$(this).attr('name')} cannot be empty</p>`)
+                .addClass('errorMsg').insertAfter($(this));
+                }
+            });
+        //Email Vailidation
+        if(emailValidation(emailInput.val())===false&&emailInput.val() !==""){
+            $('.signup-form-section form input').attr('margin-top','16px');
+            $(`<p>Looks like this is not an email</p>`)
+            .addClass('errorMsg').insertAfter(emailInput);
+        };
+            //PassWord Validation(with password suggestion)
+            //TODO have this function/module working
+     
+        e.preventDefault();
+    })
+
+})
